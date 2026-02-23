44ecb1362b4163def9e9d9d34572c013f1d414a2
NewcomerATL <andersontirza@gmail.com>
2026-02-23 20:42:53 +0700

    Penambahan SonarCloud dan ScoreCard

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     jacoco
     id("org.springframework.boot") version "3.2.2"
     id("io.spring.dependency-management") version "1.1.7"
+    id ("org.sonarqube") version "5.0.0.4638"
 }
 
 group = "id.ac.ui.cs.advprog"
@@ -77,4 +78,13 @@ tasks.test{
 
 tasks.jacocoTestReport{
     dependsOn(tasks.test)
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "A-Anderson-Tirza-Liman-2406355893_Modul-1-2-3-Coding-Standards-CI-CD")
+        property("sonar.organization", "a-anderson-tirza-liman-2406355893")
+        property("sonar.host.url", "https://sonarcloud.io")
+        property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/test/jacocoTestReport.xml")
+    }
 }
